fdb1da1c942584ce41e101e412c426e38a3f55e3
prashantojha2010 <prashantojha2706@gmail.com>
2018-12-30 02:30:30 +0530

    added

--- src/main/java/Pages/NextPage.java
+++ src/main/java/Pages/NextPage.java
@@ -4,11 +4,8 @@ public class NextPage {
 
 public static void nextPage() {
 System.out.println("welcome to login Page");
-System.out.println("welcome to login Pages");
-System.out.println("welcome to login Pages  new");
+System.out.println("welcome to login of Registration Page");
 }
-
-public void NavigationToFormerPage() {
-	
+public void NavigationToFormerPage() {	
 }
 }
