946e504fa06a37afb1e7af1d2211fe6b4fcf8fe4
Prashant Ojha <45822382+prashantojha2010@users.noreply.github.com>
2018-12-30 02:26:39 +0530

    Update NextPage.java

--- src/main/java/Pages/NextPage.java
+++ src/main/java/Pages/NextPage.java
@@ -5,6 +5,7 @@ public class NextPage {
 public static void nextPage() {
 System.out.println("welcome to login Page");
 System.out.println("welcome to login Pages");
+System.out.println("welcome to login Pages  new");
 }
 
 public void NavigationToFormerPage() {
