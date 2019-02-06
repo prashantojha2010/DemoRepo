026b21e6a5b3a08159a49d4e3f903e52de93f8be
Prashant Ojha <45822382+prashantojha2010@users.noreply.github.com>
2019-02-07 02:51:56 +0530

    Update LoginTest.java

--- src/test/java/Test/LoginTest.java
+++ src/test/java/Test/LoginTest.java
@@ -5,5 +5,6 @@ public class LoginTest {
 public void login() {
 System.out.println("Welcome to login");	
 System.out.println("I love reading ");
+System.out.println("I love writing in newspapers");
 }
 }
